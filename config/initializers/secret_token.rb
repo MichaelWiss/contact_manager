572d9ff20dc192294853da459130bff443aedda2
Michael Wiss <michael.wiss@gmail.com>
2014-04-22 13:00:36 -0400

    added rspec, unicorn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactManager::Application.config.secret_key_base = 'd5569e3f573a15c640bb058f49f662e2a4b6572c88da9d6f14981cef06a1ea859f0dc74ec47ba65c32f6d7e8dbb28de7469ef20506e55b917e6491101bd3d9cf'
