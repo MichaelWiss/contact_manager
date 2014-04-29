3eb668e6051912630e1de405595218b69d3af018
Michael Wiss <michael.wiss@gmail.com>
2014-04-29 10:31:42 -0400

    Make person spec more succinct

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,6 +4,9 @@ describe Person do
 let(:person) do
   Person.new(first_name: 'Alice', last_name: 'Smith')
 end
+  it 'is valid' do
+  expect(person).to be_valid
+end
   it 'is invalid without a first name' do
   person.first_name = nil
   expect(person).to_not be_valid
